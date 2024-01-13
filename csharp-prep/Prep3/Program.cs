f0bc2a666cd97101916c1f221f7390078367f150
Katlin Denney <den23002@byui.edu>
2024-01-12 19:31:01 -0700

    working on prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,35 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Console.Write("what is the magic number? ");
+        string userInput = Console.ReadLine();
+        int magicNumber = int.Parse(userInput);
+        string userGuess;
+        int guess;
+        
+        do
+        {   
+            Console.Write("what is your guess?");
+            userGuess = Console.ReadLine();
+            guess = int.Parse(userGuess);
+            
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("guess higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("guess lower");
+            }
+            else
+            {
+                Console.Write("something went wrong");
+            }
+        } while (guess != magicNumber);
+
+        Console.Write("You guessed it!");
+
+       
     }
 }
