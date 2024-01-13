b2b16298a713158e55df38ccdd3474e7fdb2ab5e
Katlin Denney <den23002@byui.edu>
2024-01-13 12:34:19 -0700

    prep5 in process

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        string name = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int number = GetSquareNumber();
+        int numsquared = SquareNumber(number);
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Hello, Welcome to the Prep5 Program. Glad you're here! ");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Enter your favorite number: ");
+        string userInput = Console.ReadLine();
+        int userNumber = int.Parse(userInput);
+        return userNumber;
+
+    }
+    static int GetSquareNumber()
+    {
+        Console.Write("Enter a number to square: ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+        return number;
+    }
+    static int SquareNumber(int number)
+    {
+        
+        return number * number;
+
+    }
+    static void DisplayResult()
+    {
+        Console.WriteLine($"{}{}{}")
     }
 }
